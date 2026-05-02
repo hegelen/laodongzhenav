eb917502d49fbb3d01e41c9f964e6a2e7ed50562
GitHub Action <action@github.com>
2026-05-02 11:33:15 +0000

    自动更新最新文章

---
+++ latest-articles.js
@@ -0,0 +1,64 @@
+const latestArticlesByYear = {
+  "2025": [
+    {
+      "name": "VeryJack",
+      "title": "是时候，走出象牙塔了",
+      "url": "https://veryjack.com/life/step_out_of_comfort_zone/",
+      "date": "2026-03-28"
+    },
+    {
+      "name": "酷壳",
+      "title": "是微服务架构不香还是云不香？",
+      "url": "https://coolshell.cn/articles/22422.html",
+      "date": "2023-05-08"
+    },
+    {
+      "name": "joyqi",
+      "title": "彻底放弃收费 SSL 证书",
+      "url": "https://joyqi.com/cloud/drop-paid-ssl-cert.html",
+      "date": "2025-12-12"
+    },
+    {
+      "name": "龙天小筑",
+      "title": "Stable Diffusion Webui无显卡安装",
+      "url": "https://www.dragongod.net/2023/02/install-stable-diffusion-webui-without-gpu/?utm_source=rss&utm_medium=rss&utm_campaign=install-stable-diffusion-webui-without-gpu",
+      "date": "2023-02-28"
+    },
+    {
+      "name": "礼印外盒",
+      "title": "出现又离开",
+      "url": "https://liyinwaihe.com/341.html",
+      "date": "2025-12-08"
+    },
+    {
+      "name": "小z",
+      "title": "5分钟拥有自己的“小龙虾”：PicoClaw Docker 部署教程",
+      "url": "https://blog.xiaoz.org/archives/22928",
+      "date": "2026-04-07"
+    },
+    {
+      "name": "小胡",
+      "title": "男人",
+      "url": "https://hmilyld.com/archives/55.html",
+      "date": "2024-11-05"
+    },
+    {
+      "name": "无标题文档",
+      "title": "在 VSCode 和 Claude Code 接入 Ling 1T 模型，以及些使用心得",
+      "url": "https://www.gracecode.com/posts/3207.html",
+      "date": "2026-02-27"
+    },
+    {
+      "name": "远梦",
+      "title": "可那些你以为的观众，其实根本没买票入场。",
+      "url": "https://blog.qoz.cc/2179/",
+      "date": "2025-07-25"
+    },
+    {
+      "name": "科学空间",
+      "title": "MuP之上：4. 坚守参数的稳定性",
+      "url": "https://kexue.fm/archives/11729",
+      "date": "2026-04-24"
+    }
+  ]
+};
