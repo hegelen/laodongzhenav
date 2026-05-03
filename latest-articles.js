b0fd1a426ce57e404ed412df8b79709e534f5773
GitHub Action <action@github.com>
2026-05-03 11:32:32 +0000

    自动更新文章

--- latest-articles.js
+++ latest-articles.js
@@ -1,93 +1,95 @@
 // ==================== latest-articles.js ====================
-// 抓取日期: 2026/5/3 15:22:06
+// 抓取日期: 5/3/2026, 11:08:00 AM
 // 只抓取最近7天内的文章，共 10 篇
 // 目标 10 篇，实际 10 篇
 
 const latestArticlesByYear = {
-  "2004": [
+  "2008": [
     {
-      "name": "龙哥",
-      "year": "04",
-      "title": "用ipabc.de做了一个ip查询网站",
-      "url": "https://long.ge/18063.html",
+      "name": "军",
+      "year": "08",
+      "title": "难得正版Windows又打骨折！白菜价",
+      "url": "https://me.xu19.com/windows11-big-discount/",
+      "date": "2026-05-01"
+    }
+  ],
+  "2009": [
+    {
+      "name": "老张",
+      "year": "09",
+      "title": "我只会瞎折腾！暨兰空图床上传图片失败解决方法！",
+      "url": "https://laozhang.org/archives/4056.html",
       "date": "2026-04-28"
     }
   ],
-  "2007": [
+  "2015": [
     {
-      "name": "织梦岁月",
-      "year": "07",
-      "title": "2026年4月总结",
-      "url": "https://zhujianlin.com/archives/1879.html",
-      "date": "2026-04-30"
+      "name": "泽泽社长",
+      "year": "15",
+      "title": "从MacbookAir换到MacbookPro",
+      "url": "https://blog.aihaonv.com/macbookair-to-macbookpro.html",
+      "date": "2026-04-28"
     }
   ],
-  "2010": [
+  "2016": [
     {
-      "name": "雅余",
-      "year": "10",
-      "title": "动物朋友",
-      "url": "https://yayu.net/6218.html",
-      "date": "2026-04-30"
+      "name": "woodu",
+      "year": "16",
+      "title": "JVS时代的经典乘客——《太鼓之达人 6》",
+      "url": "https://woodu.me/jvsshidaidejingdiancheng/",
+      "date": "2026-05-02"
     }
   ],
   "2017": [
     {
-      "name": "马春杰杰",
+      "name": "染念",
       "year": "17",
-      "title": "吐槽：记录一次站点被Meta爬虫刷掉400G流量的排查过程",
-      "url": "https://www.machunjie.com/web/2120.html",
-      "date": "2026-04-29"
+      "title": "lens|打造最强的个人LLM聚合网关系统",
+      "url": "https://dyedd.cn/posts/lens_api",
+      "date": "2026-04-28"
+    },
+    {
+      "name": "染念",
+      "year": "17",
+      "title": "lens|打造最强的个人LLM聚合网关系统",
+      "url": "https://dyedd.cn/posts/lens_api",
+      "date": "2026-04-28"
+    }
+  ],
+  "2018": [
+    {
+      "name": "运维咖啡吧",
+      "year": "18",
+      "title": "游记｜在宜兴度过一个平淡的假期",
+      "url": "https://blog.ops-coffee.com/r/city-china-jiangsu-wuxi-yixing-tuanjiu.html",
+      "date": "2026-04-26"
     }
   ],
   "2019": [
     {
-      "name": "流情",
+      "name": "李锋镝",
       "year": "19",
-      "title": "通过openclaw装hermes，倒反天罡",
-      "url": "https://liuqingwushui.top/archives/155/",
-      "date": "2026-04-30"
+      "title": "踩坑60+次后，我终于搞懂 Claude Skill 怎么写才会真的触发",
+      "url": "https://www.lifengdi.com/ren-gong-zhi-neng/4698",
+      "date": "2026-04-27"
+    }
+  ],
+  "2022": [
+    {
+      "name": "倦意",
+      "year": "22",
+      "title": "Juanyi Chat：现代化实时的聊天室，无需框架依赖，开箱即用。",
+      "url": "https://jyblog.cn/5465/",
+      "date": "2026-05-02"
     }
   ],
   "2023": [
     {
-      "name": "鹿泽",
+      "name": "宗宗酱",
       "year": "23",
-      "title": "在谷歌商家资料中选择并优化分类",
-      "url": "https://www.bailuze.com/24049.html",
-      "date": "2026-05-02"
-    },
-    {
-      "name": "维奥洛",
-      "year": "23",
-      "title": "恍惚间，自己分不清用的 win11 还是 arch",
-      "url": "https://vioulo.com/archives/win11-or-arch.html",
-      "date": "2026-04-30"
-    }
-  ],
-  "2024": [
-    {
-      "name": "ImQi1",
-      "year": "24",
-      "title": "为了一个女孩，我哭了一整晚",
-      "url": "https://imqi1.com/content/note/1009",
-      "date": "2026-04-27"
-    }
-  ],
-  "2025": [
-    {
-      "name": "心记",
-      "year": "25",
-      "title": "自运营的博客之家论坛已上线",
-      "url": "https://blog.moodlog.cn/archives/16/",
-      "date": "2026-04-29"
-    },
-    {
-      "name": "巴适小馨",
-      "year": "25",
-      "title": "善待和尊重处在低谷期的奋进者",
-      "url": "https://p4z.cn/archives/98.html",
-      "date": "2026-04-27"
+      "title": "这是刺激我消费啊",
+      "url": "https://ygz.ink/archives/4785.html",
+      "date": "2026-05-03"
     }
   ]
 };
