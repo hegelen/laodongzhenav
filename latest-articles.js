14e3b50030759b0266875a7bc5ad1a7c46f540f8
GitHub Action <action@github.com>
2026-05-13 20:49:19 +0000

    更新文章

--- latest-articles.js
+++ latest-articles.js
@@ -1,5 +1,5 @@
 // ==================== latest-articles.js ====================
-// 抓取日期: 5/12/2026, 8:44:16 PM
+// 抓取日期: 5/13/2026, 8:49:19 PM
 // 只抓取最近14天内的文章，共 10 篇
 // 目标 10 篇，实际 10 篇
 
@@ -22,21 +22,21 @@ const latestArticlesByYear = {
       "date": "2026-05-12"
     }
   ],
-  "2009": [
+  "2010": [
     {
-      "name": "老卫",
-      "year": "09",
-      "title": "历时三年《鸿蒙系统（HarmonyOS）移动开发实战》简介",
-      "url": "https://waylau.com/about-harmonyos-short-video-book/",
-      "date": "2026-05-01"
+      "name": "忆往事",
+      "year": "10",
+      "title": "补牙",
+      "url": "https://zhou.ge/2026-05-05.html",
+      "date": "2026-05-05"
     }
   ],
-  "2016": [
+  "2011": [
     {
-      "name": "幻影",
-      "year": "16",
-      "title": "洗衣机复活记",
-      "url": "https://blog.52hyjs.com/post-1269.html",
+      "name": "耳朵的主人",
+      "year": "11",
+      "title": "妈祖诞辰1066周年｜善坛妈祖庙祭典",
+      "url": "https://www.edzbe.com/book/diary/20260509.html",
       "date": "2026-05-11"
     }
   ],
@@ -46,6 +46,15 @@ const latestArticlesByYear = {
       "year": "18",
       "title": "Ubuntu 26.04 Desktop使用体验",
       "url": "https://blog.keepchen.com/a/Ubuntu-26.04-Desktop-User-Experience.html",
+      "date": "2026-05-10"
+    }
+  ],
+  "2019": [
+    {
+      "name": "流情",
+      "year": "19",
+      "title": "5.10 母亲节亦是加班日",
+      "url": "https://liuqingwushui.top/archives/156/",
       "date": "2026-05-10"
     }
   ],
@@ -60,36 +69,27 @@ const latestArticlesByYear = {
   ],
   "2023": [
     {
-      "name": "小野",
+      "name": "Siwei Li",
       "year": "23",
-      "title": "狐蒂云疑似准备跑路：低价云服务器的水有多深",
-      "url": "https://lb5.net/253.html",
-      "date": "2026-05-06"
+      "title": "赫尔米勒embody",
+      "url": "https://siwei.li/he-er-mi-le-embody/",
+      "date": "2026-05-08"
     },
     {
       "name": "宗宗酱",
       "year": "23",
-      "title": "马上2026世界杯来了，买体彩不？",
-      "url": "https://ygz.ink/archives/4884.html",
-      "date": "2026-05-12"
-    }
-  ],
-  "2024": [
-    {
-      "name": "ImQi1",
-      "year": "24",
-      "title": "跨越了两千公里，只为见到你",
-      "url": "https://imqi1.com/content/note/1010",
-      "date": "2026-05-10"
+      "title": "真他妈缺德东西",
+      "url": "https://ygz.ink/archives/4893.html",
+      "date": "2026-05-13"
     }
   ],
   "2025": [
     {
-      "name": "礼印外盒",
+      "name": "落花雨记",
       "year": "25",
-      "title": "爱于细碎烟火，悟爱的本真爱",
-      "url": "https://liyinwaihe.com/399.html",
-      "date": "2026-05-09"
+      "title": "牛马生活",
+      "url": "https://www.luohuayu.cn/68.html",
+      "date": "2026-05-08"
     }
   ]
 };
