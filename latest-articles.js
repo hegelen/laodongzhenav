2ccc561b59b48b5b43d7b1be1eb8e254c72081dd
GitHub Action <action@github.com>
2026-06-03 21:55:20 +0000

    更新文章

--- latest-articles.js
+++ latest-articles.js
@@ -1,40 +1,65 @@
 // ==================== latest-articles.js ====================
-// 抓取日期: 6/3/2026, 8:19:32 AM
+// 抓取日期: 6/3/2026, 9:55:20 PM
 // 只抓取最近14天内的文章，共 10 篇
 // 目标 10 篇，实际 10 篇
 
 const latestArticlesByYear = {
-  "2007": [
+  "2002": [
     {
-      "name": "织梦岁月",
-      "year": "07",
-      "title": "尝鲜，我以后还是不要尝鲜了",
-      "url": "https://zhujianlin.com/archives/1992.html",
-      "date": "2026-06-02"
+      "name": "seth",
+      "year": "02",
+      "title": "Professionals know how to talk about it",
+      "url": "https://seths.blog/2026/06/professionals-know-how-to-talk-about-it/",
+      "date": "2026-06-03"
     }
   ],
-  "2013": [
+  "2004": [
     {
-      "name": "林羽凡",
-      "year": "13",
-      "title": "做个简单总结：今天完成了“林羽凡笔记”agent中的“webSearch”工具",
-      "url": "https://www.linyufan.com/post/5834",
-      "date": "2026-06-02"
+      "name": "kaix.in",
+      "year": "04",
+      "title": "咬文嚼字",
+      "url": "https://kaix.in/2026/0603/",
+      "date": "2026-06-03"
+    }
+  ],
+  "2005": [
+    {
+      "name": "Gea-Suan Lin",
+      "year": "05",
+      "title": "走 RFC 2136 更新 DNS record 的 Dynamic DNS 服務",
+      "url": "https://blog.gslin.org/archives/2026/06/04/13057/%e8%b5%b0-rfc-2136-%e6%9b%b4%e6%96%b0-dns-record-%e7%9a%84-dynamic-dns-%e6%9c%8d%e5%8b%99/",
+      "date": "2026-06-03"
+    },
+    {
+      "name": "春田",
+      "year": "05",
+      "title": "１０７７．人生には岐路がある",
+      "url": "http://kaikeimura.way-nifty.com/blog/2026/06/post-0d00b8.html",
+      "date": "2026-06-03"
+    }
+  ],
+  "2006": [
+    {
+      "name": "随风沐虐",
+      "year": "06",
+      "title": "天涯社区恢复访问，你登录了吗？",
+      "url": "https://www.munue.com/799.html",
+      "date": "2026-06-03"
     }
   ],
   "2017": [
     {
-      "name": "SimonAKing",
+      "name": "碎言",
       "year": "17",
-      "title": "AI 互动影游全球深度报告",
-      "url": "https://simonaking.com/blog/ai-interactive-film-game-report/",
-      "date": "2026-06-02"
+      "title": "如何选择趁手的兵器（大模型和配套的工具）",
+      "url": "https://www.suiyan.cc/blog/20260603200000",
+      "date": "2026-06-03"
     },
     {
-      "name": "科学空间",
+      "name": "串串狗小刊",
       "year": "17",
-      "title": "为什么官方版Muon比MuP版多出一个max(1, ⋅)？",
-      "url": "https://kexue.fm/archives/11772",
+      "title": "SEO 之 UGC 不做落地页和 ADS 无效流量解决方法",
+      "url": "https://www.ccgxk.com/codeother/767.html",
       "date": "2026-06-03"
     }
   ],
@@ -42,49 +67,26 @@ const latestArticlesByYear = {
     {
       "name": "初然忆",
       "year": "20",
-      "title": "鼠疫2：第一次连续开出三个红",
-      "url": "https://www.imcry.vip/post/2026-6-3-301/",
-      "date": "2026-06-02"
+      "title": "芜湖！收到好站网的赠品了",
+      "url": "https://www.imcry.vip/post/2026-6-3-2126/",
+      "date": "2026-06-03"
     }
   ],
   "2021": [
     {
       "name": "DevNow",
       "year": "21",
-      "title": "Product Hunt 每日热榜 | 2026-06-02",
-      "url": "https://www.laughingzhu.cn/posts/ph-daily-2026-06-02",
-      "date": "2026-06-02"
-    },
-    {
-      "name": "阿锋的小宇宙",
-      "year": "21",
-      "title": "二〇二六年五月总结：颓态总会过去吧",
-      "url": "https://feng.pub/0620269616.html",
-      "date": "2026-06-02"
+      "title": "Product Hunt 每日热榜 | 2026-06-03",
+      "url": "https://www.laughingzhu.cn/posts/ph-daily-2026-06-03",
+      "date": "2026-06-03"
     }
   ],
   "2023": [
     {
-      "name": "宗宗酱",
+      "name": "湘铭",
       "year": "23",
-      "title": "轻轨上偶遇也门小哥",
-      "url": "https://ygz.ink/archives/5134.html",
-      "date": "2026-06-02"
-    },
-    {
-      "name": "鹿泽",
-      "year": "23",
-      "title": "让信息型SEO内容为搜索意图发挥作用",
-      "url": "https://www.bailuze.com/24115.html",
-      "date": "2026-06-03"
-    }
-  ],
-  "2026": [
-    {
-      "name": "华立",
-      "year": "26",
-      "title": "打算重构墨迹博客（InkBlog）",
-      "url": "https://www.renhuali.cn/journal/cmpxdw1ac000xchs4ybm9m416",
+      "title": "安家之后，岁岁清欢",
+      "url": "https://xiangming.site/1131.html",
       "date": "2026-06-03"
     }
   ]
