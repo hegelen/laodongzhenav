5b7a962387067cdd95c1245a9b471670530f19ea
GitHub Action <action@github.com>
2026-07-06 21:02:01 +0000

    更新文章

--- latest-articles.js
+++ latest-articles.js
@@ -1,179 +1,171 @@
 // ==================== latest-articles.js ====================
-// 抓取日期: 7/5/2026, 8:25:47 PM
+// 抓取日期: 7/6/2026, 9:02:00 PM
 // 只抓取最近14天内的文章，共 20 篇
 // 目标 20 篇，实际 20 篇
 
 const latestArticlesByYear = {
-  "2000": [
-    {
-      "name": "Luca",
-      "year": "00",
-      "title": "翻星标旧文读反馈之道",
-      "url": "https://wlj.me/notes/sp-note-20260705-144935/",
-      "date": "2026-07-05"
-    }
-  ],
   "2002": [
     {
       "name": "seth",
       "year": "02",
-      "title": "The urgency paradox",
-      "url": "https://seths.blog/2026/07/the-urgency-paradox/",
-      "date": "2026-07-05"
+      "title": "Facts and feelings",
+      "url": "https://seths.blog/2026/07/facts-and-feelings/",
+      "date": "2026-07-06"
     }
   ],
   "2004": [
     {
       "name": "TonyBai",
       "year": "04",
-      "title": "五年，三篇文章，一个我一直没真正解决的问题",
-      "url": "https://tonybai.com/2026/07/05/go-private-modules-lessons-learned/",
-      "date": "2026-07-04"
+      "title": "5 分钟上手 gvu：把 vanity import path 这件事，从“半天运维”变成一条命令",
+      "url": "https://tonybai.com/2026/07/06/get-started-with-gvu-in-5-minutes/",
+      "date": "2026-07-05"
+    },
+    {
+      "name": "我的天",
+      "year": "04",
+      "title": "确定荣事达小冰箱",
+      "url": "http://www.xlanda.net/posts/22311",
+      "date": "2026-07-06"
     },
     {
       "name": "竹炉山房",
       "year": "04",
-      "title": "史上最sb裁判",
-      "url": "https://synyan.cn/t/63089",
-      "date": "2026-07-05"
+      "title": "中公教育",
+      "url": "https://synyan.cn/t/63094",
+      "date": "2026-07-06"
     }
   ],
   "2005": [
     {
-      "name": "王志勇",
-      "year": "05",
-      "title": "误操作导致部分数据丢失",
-      "url": "http://www.auiou.com/relevant/00002166.jsp",
-      "date": "2026-07-05"
-    },
-    {
       "name": "ACEVS",
       "year": "05",
-      "title": "红李子",
-      "url": "https://acevs.com/5083/",
-      "date": "2026-07-05"
+      "title": "游泳池",
+      "url": "https://acevs.com/5085/",
+      "date": "2026-07-06"
     }
   ],
   "2006": [
     {
-      "name": "傻猫",
+      "name": "忘记了回忆",
       "year": "06",
-      "title": "中文版Superpowers使用方法",
-      "url": "https://www.samool.com/52680.html",
-      "date": "2026-07-05"
-    }
-  ],
-  "2007": [
-    {
-      "name": "不靠谱颜论",
-      "year": "07",
-      "title": "地铁站的马赛克墙，究竟是随机还是有序？",
-      "url": "https://yanlinlin.cn/2026/07/05/is-the-pattern-random/",
-      "date": "2026-07-05"
+      "title": "停电",
+      "url": "https://ltmltm.cn/bk/1472.html",
+      "date": "2026-07-06"
     }
   ],
   "2013": [
     {
       "name": "林羽凡",
       "year": "13",
-      "title": "我还是为我的任性付出的代价",
-      "url": "https://www.linyufan.com/post/5881",
-      "date": "2026-07-05"
+      "title": "《新浪云终止服务》了，又一个云服务平台倒下了",
+      "url": "https://www.linyufan.com/post/5884",
+      "date": "2026-07-06"
     }
   ],
-  "2016": [
+  "2015": [
     {
-      "name": "ximi",
-      "year": "16",
-      "title": "一个简易的本地web图库预览页面",
-      "url": "https://www.ximi.me/post-6044.html",
-      "date": "2026-07-05"
+      "name": "WordPress 知识宝库",
+      "year": "15",
+      "title": "구글 사이트킷 'Unable to retrieve site connection' 오류 해결 방법",
+      "url": "https://www.thewordcracker.com/basic/%ea%b5%ac%ea%b8%80-%ec%82%ac%ec%9d%b4%ed%8a%b8%ed%82%b7-%ec%97%b0%ea%b2%b0-%ec%98%a4%eb%a5%98-%ed%95%b4%ea%b2%b0/",
+      "date": "2026-07-06"
     }
   ],
   "2017": [
     {
-      "name": "串串狗小刊",
+      "name": "科学空间",
       "year": "17",
-      "title": "Web 出海之外链提交频率和如何不花 Token 处理代码",
-      "url": "https://www.ccgxk.com/codeother/799.html",
-      "date": "2026-07-05"
+      "title": "让炼丹更科学一些（七）：步长调度与权重平均",
+      "url": "https://kexue.fm/archives/11804",
+      "date": "2026-07-06"
     },
     {
-      "name": "SimonAKing",
+      "name": "串串狗小刊",
       "year": "17",
-      "title": "模型适配权：Anthropic 藏得最深的垄断",
-      "url": "https://simonaking.com/blog/anthropic-model-adaptation-monopoly/",
-      "date": "2026-07-05"
+      "title": "Codex 敞开用的技巧和谷歌趋势的参考词",
+      "url": "https://www.ccgxk.com/codeother/800.html",
+      "date": "2026-07-06"
+    },
+    {
+      "name": "碎言",
+      "year": "17",
+      "title": "AI自动回复评论，你怎么看？",
+      "url": "https://www.suiyan.cc/blog/20260706192737",
+      "date": "2026-07-06"
     }
   ],
   "2018": [
     {
-      "name": "运维咖啡吧",
+      "name": "秋葵",
       "year": "18",
-      "title": "游记｜吹17度江风，吃地道豆腐包，建德3天2晚散漫自驾",
-      "url": "https://blog.ops-coffee.com/r/city-china-zhejiang-hangzhou-jiande-yanzhougucheng.html",
-      "date": "2026-07-05"
+      "title": "GitHub一周热点",
+      "url": "https://qiukui-note.happy365.day/20260706/github-trends-weekly/",
+      "date": "2026-07-06"
     }
   ],
-  "2020": [
+  "2019": [
     {
-      "name": "呆哥",
-      "year": "20",
-      "title": "网站迁移完成，恢复访问",
-      "url": "https://www.dalao.net/thread-60603.htm",
-      "date": "2026-07-05"
-    }
-  ],
-  "2021": [
+      "name": "吴诗涛",
+      "year": "19",
+      "title": "记远程办公新起点",
+      "url": "https://shitao5.org/posts/remote-work-new-beginning/",
+      "date": "2026-07-06"
+    },
     {
-      "name": "DevNow",
-      "year": "21",
-      "title": "Product Hunt 每日热榜 | 2026-07-05",
-      "url": "https://www.laughingzhu.cn/posts/ph-daily-2026-07-05",
-      "date": "2026-07-05"
-    }
-  ],
-  "2022": [
+      "name": "流情",
+      "year": "19",
+      "title": "【景点篇】青城后山",
+      "url": "https://liuqingwushui.top/archives/169/",
+      "date": "2026-07-06"
+    },
     {
-      "name": "倦意",
-      "year": "22",
-      "title": "演唱会退票经历分享 退票需要收取高额手续费 维权的过程更难",
-      "url": "https://jyblog.com/5747/",
-      "date": "2026-07-05"
+      "name": "呢喃",
+      "year": "19",
+      "title": "世界，您好！",
+      "url": "https://ninan.me/2026/07/06/hello-world/",
+      "date": "2026-07-06"
     }
   ],
   "2023": [
     {
-      "name": "宗宗酱",
-      "year": "23",
-      "title": "📷“拍照特种兵”的下午",
-      "url": "https://ygz.ink/archives/5457.html",
-      "date": "2026-07-05"
-    },
-    {
       "name": "小归客",
       "year": "23",
-      "title": "个人主页旧版本开源",
-      "url": "https://xgk.pw/archives/305/",
-      "date": "2026-07-05"
+      "title": "小归客个人主页程序2026版本",
+      "url": "https://xgk.pw/archives/310/",
+      "date": "2026-07-06"
+    },
+    {
+      "name": "按钮与磁带",
+      "year": "23",
+      "title": "镖人：风起大漠",
+      "url": "https://jefftay.com/movies/blades-of-the-guardians",
+      "date": "2026-07-06"
     }
   ],
   "2025": [
     {
       "name": "Marcus",
       "year": "25",
-      "title": "2026.07.05 博客阅读周记",
-      "url": "https://www.immarcus.com/blog/weekly-20260705",
-      "date": "2026-07-05"
+      "title": "世界奇妙物语",
+      "url": "https://www.immarcus.com/blog/yonimo-kimyona-monogatari",
+      "date": "2026-07-06"
     }
   ],
   "2026": [
     {
-      "name": "麦子的思绪",
+      "name": "一个博客",
       "year": "26",
-      "title": "喝酒误事",
-      "url": "https://mzdsx.com/2026/07/05/%e5%96%9d%e9%85%92%e8%af%af%e4%ba%8b/",
-      "date": "2026-07-04"
+      "title": "说说：时隔很久没有走进电影院，周星驰新作《功夫女足》定档 7 月 11 日，…",
+      "url": "https://yigeboke.com/shuoshuo.php#shuo_34",
+      "date": "2026-07-06"
+    },
+    {
+      "name": "华立",
+      "year": "26",
+      "title": "自古不变的五大赚钱赛道",
+      "url": "https://www.renhuali.cn/journal/cmr8mud1301twchyjc70i3kpr",
+      "date": "2026-07-06"
     }
   ]
 };
