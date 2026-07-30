4f8e28135cd3a14647e7b219df182dc1c6ce402f
GitHub Action <action@github.com>
2026-07-30 20:23:06 +0000

    更新文章

--- latest-articles.js
+++ latest-articles.js
@@ -1,5 +1,5 @@
 // ==================== latest-articles.js ====================
-// 抓取日期: 7/29/2026, 8:22:13 PM
+// 抓取日期: 7/30/2026, 8:23:06 PM
 // 只抓取最近14天内的文章，共 20 篇
 // 目标 20 篇，实际 20 篇
 
@@ -8,170 +8,168 @@ const latestArticlesByYear = {
     {
       "name": "seth",
       "year": "02",
-      "title": "Eager to give up agency",
-      "url": "https://seths.blog/2026/07/eager-to-give-up-agency/",
-      "date": "2026-07-29"
+      "title": "Organizations that need you",
+      "url": "https://seths.blog/2026/07/organizations-that-need-you/",
+      "date": "2026-07-30"
     }
   ],
   "2004": [
     {
       "name": "我的天",
       "year": "04",
-      "title": "桌底抽屉高度碎碎念",
-      "url": "http://www.xlanda.net/posts/22363",
-      "date": "2026-07-29"
+      "title": "再谈桌底抽屉",
+      "url": "http://www.xlanda.net/posts/22365",
+      "date": "2026-07-30"
+    },
+    {
+      "name": "小猪的窝",
+      "year": "04",
+      "title": "消失的朋友圈",
+      "url": "https://ezo.biz/Diary/1524.html",
+      "date": "2026-07-30"
     }
   ],
   "2005": [
     {
       "name": "Gea-Suan Lin",
       "year": "05",
-      "title": "Hacker News 上看到運動褲用的繩結 Slipped Lapp Knot",
-      "url": "https://blog.gslin.org/archives/2026/07/29/13130/hacker-news-%e4%b8%8a%e7%9c%8b%e5%88%b0%e9%81%8b%e5%8b%95%e8%a4%b2%e7%94%a8%e7%9a%84%e7%b9%a9%e7%b5%90-slipped-lapp-knot/",
-      "date": "2026-07-29"
-    },
-    {
-      "name": "ACEVS",
-      "year": "05",
-      "title": "晾碗架",
-      "url": "https://acevs.com/5134/",
-      "date": "2026-07-29"
-    }
-  ],
-  "2006": [
-    {
-      "name": "随风沐虐",
-      "year": "06",
-      "title": "详列东野圭吾106部作品完整清单",
-      "url": "https://www.munue.com/810.html",
-      "date": "2026-07-29"
-    }
-  ],
-  "2008": [
-    {
-      "name": "军",
-      "year": "08",
-      "title": "女儿学画画1｜素描-相机中荷花",
-      "url": "https://me.xu19.com/daughter-learning-to-draw-part-1-sketch-lotus-in-camera/",
-      "date": "2026-07-29"
+      "title": "伊斯蘭革命衛隊 IRGC 打 AWS 在巴林的設施",
+      "url": "https://blog.gslin.org/archives/2026/07/31/13131/%e4%bc%8a%e6%96%af%e8%98%ad%e9%9d%a9%e5%91%bd%e8%a1%9b%e9%9a%8a-irgc-%e6%89%93-aws-%e5%9c%a8%e5%b7%b4%e6%9e%97%e7%9a%84%e8%a8%ad%e6%96%bd/",
+      "date": "2026-07-30"
     }
   ],
   "2009": [
     {
-      "name": "张鑫旭",
+      "name": "老卫",
       "year": "09",
-      "title": "Screen.isExtended、getScreenDetails与多显示器开发",
-      "url": "https://www.zhangxinxu.com/wordpress/2026/07/windows-getscreendetails-isextended/",
-      "date": "2026-07-29"
+      "title": "SonarQube从GitLab创建项目",
+      "url": "https://waylau.com/sonarqube-create-project-from-gitlab/",
+      "date": "2026-07-30"
+    }
+  ],
+  "2010": [
+    {
+      "name": "麦麦同学",
+      "year": "10",
+      "title": "山东泰山队北看台某些球迷真是孙子",
+      "url": "https://www.mmtx.net/1179.html",
+      "date": "2026-07-30"
     }
   ],
   "2011": [
     {
       "name": "小癞子",
       "year": "11",
-      "title": "泽西岛的J车牌: 熟悉的英国味, 不一样的海岛风情",
-      "url": "https://justyy.com/archives/72298",
-      "date": "2026-07-29"
+      "title": "加入微软五周年: 以水晶为证, 致敬一路成长",
+      "url": "https://justyy.com/archives/72310",
+      "date": "2026-07-30"
     }
   ],
   "2013": [
     {
       "name": "林羽凡",
       "year": "13",
-      "title": "今天没有减肥餐，买了一套武林秘籍",
-      "url": "https://www.linyufan.com/post/5922",
-      "date": "2026-07-29"
-    }
-  ],
-  "2014": [
+      "title": "不懂CAD到底能不能用Ai画图",
+      "url": "https://www.linyufan.com/post/5925",
+      "date": "2026-07-30"
+    },
     {
-      "name": "缙哥哥",
-      "year": "14",
-      "title": "智谱GLM模型免费2000万Tokens算力，最高每月可得2亿",
-      "url": "https://www.dujin.org/23801.html",
-      "date": "2026-07-29"
-    }
-  ],
-  "2015": [
-    {
-      "name": "WordPress 知识宝库",
-      "year": "15",
-      "title": "워드프레스 악성 봇 차단하여 서버 과부하 방지하기",
-      "url": "https://www.thewordcracker.com/basic/%ec%9b%8c%eb%93%9c%ed%94%84%eb%a0%88%ec%8a%a4-%ec%95%85%ec%84%b1-%eb%b4%87-%ec%b0%a8%eb%8b%a8%ed%95%98%ec%97%ac-%ec%84%9c%eb%b2%84-%ea%b3%bc%eb%b6%80%ed%95%98-%eb%b0%a9%ec%a7%80/",
-      "date": "2026-07-29"
+      "name": "小z",
+      "year": "13",
+      "title": "美国第一证券开户 + WISE入金 + 购买美股全流程分享",
+      "url": "https://blog.xiaoz.org/archives/23286",
+      "date": "2026-07-30"
     }
   ],
   "2017": [
     {
-      "name": "科学空间",
+      "name": "刘承",
       "year": "17",
-      "title": "解构Scaling Law：优化、架构、数据的三重奏",
-      "url": "https://kexue.fm/archives/11833",
-      "date": "2026-07-29"
-    },
-    {
-      "name": "碎言",
-      "year": "17",
-      "title": "字间 260729",
-      "url": "https://www.suiyan.cc/blog/20260729193734",
-      "date": "2026-07-29"
+      "title": "又下雨了",
+      "url": "https://czhlove.cn/rainingAgain",
+      "date": "2026-07-30"
     },
     {
       "name": "串串狗小刊",
       "year": "17",
-      "title": "向量数据库，ChromaDB 和 Qdrant 到底怎么选？",
-      "url": "https://www.ccgxk.com/codeother/825.html",
-      "date": "2026-07-29"
+      "title": "程序员不要学编程技能了，意义近似 0，直接干！",
+      "url": "https://www.ccgxk.com/emlog_dev/826.html",
+      "date": "2026-07-30"
     }
   ],
   "2019": [
     {
-      "name": "紫塞",
+      "name": "流情",
       "year": "19",
-      "title": "父亲从江苏走来：留给我的精神故乡",
-      "url": "https://zisai.com/posts/shenguohui/",
-      "date": "2026-07-29"
-    },
-    {
-      "name": "吴诗涛",
-      "year": "19",
-      "title": "AI 写给 AI 看：一种新时代形式主义",
-      "url": "https://shitao5.org/posts/ai-era-formalism/",
-      "date": "2026-07-29"
+      "title": "突来的消息，降薪的是你",
+      "url": "https://liuqingwushui.top/archives/180/",
+      "date": "2026-07-30"
     }
   ],
   "2020": [
     {
       "name": "呆哥",
       "year": "20",
-      "title": "我的giffgaff卡也拉闸了【余额已捐款】",
-      "url": "https://www.dalao.net/thread-61768.htm",
-      "date": "2026-07-29"
+      "title": "giffgaff替代品是啥",
+      "url": "https://www.dalao.net/thread-61825.htm",
+      "date": "2026-07-30"
+    },
+    {
+      "name": "資工小廢物 - JN",
+      "year": "20",
+      "title": "那些你認為的華語金曲，其實都是翻唱的！",
+      "url": "https://blog.giveanornot.com/mandarin-cover-songs/",
+      "date": "2026-07-30"
+    }
+  ],
+  "2021": [
+    {
+      "name": "记录生活",
+      "year": "21",
+      "title": "自己泡的茉莉蜜茶，甜不甜我说了算",
+      "url": "https://9sb.net/archives/i-make-my-own-jasmine-tea-its-the-final-say-whether-its-sweet-or-not.html",
+      "date": "2026-07-30"
+    },
+    {
+      "name": "喵喵小站・博客志",
+      "year": "21",
+      "title": "说说：#2026-07-30 00:07",
+      "url": "https://www.mmbkz.cn/71007da4.html",
+      "date": "2026-07-30"
+    },
+    {
+      "name": "DevNow",
+      "year": "21",
+      "title": "Product Hunt 每日热榜 | 2026-07-30",
+      "url": "https://www.laughingzhu.cn/posts/ph-daily-2026-07-30",
+      "date": "2026-07-30"
     }
   ],
   "2023": [
     {
       "name": "鹿泽",
       "year": "23",
-      "title": "标题标签：H1、H2 和 H3 HTML 标题标签的简单（但完整）指南，适用于SEO",
-      "url": "https://www.bailuze.com/24277.html",
-      "date": "2026-07-29"
+      "title": "多语言SEO优化是什么？",
+      "url": "https://www.bailuze.com/24283.html",
+      "date": "2026-07-30"
     }
   ],
   "2024": [
     {
-      "name": "ImQi1",
+      "name": "刘郎阁",
       "year": "24",
-      "title": "蜘蛛侠4，咋说呢，还是挺烂的",
-      "url": "https://imqi1.com/content/note/1019",
-      "date": "2026-07-29"
-    },
+      "title": "七月近况",
+      "url": "https://vjo.cc/1990",
+      "date": "2026-07-30"
+    }
+  ],
+  "2025": [
     {
-      "name": "三叔",
-      "year": "24",
-      "title": "苹果邮箱转outlook脚本",
-      "url": "https://www.sanshu.cn/arc/263.html",
-      "date": "2026-07-29"
+      "name": "Marcus",
+      "year": "25",
+      "title": "幕后黑手",
+      "url": "https://www.immarcus.com/blog/hidden-villain",
+      "date": "2026-07-30"
     }
   ]
 };
