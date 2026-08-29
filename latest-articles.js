abd3fc5538b5dad2ed34f71e2fafcd65984f623c
GitHub Action <action@github.com>
2026-08-29 21:42:33 +0000

    更新文章

--- latest-articles.js
+++ latest-articles.js
@@ -1,34 +1,34 @@
 // ==================== latest-articles.js ====================
-// 抓取日期: 8/29/2026, 2:12:42 AM
-// 只抓取最近14天内的文章，共 17 篇
-// 目标 20 篇，实际 17 篇
+// 抓取日期: 8/29/2026, 9:42:33 PM
+// 只抓取最近14天内的文章，共 20 篇
+// 目标 20 篇，实际 20 篇
 
 const latestArticlesByYear = {
   "2000": [
     {
       "name": "Luca",
       "year": "00",
-      "title": "旧日新装再探omarchy",
-      "url": "https://wlj.me/notes/sp-note-20260828-230026/",
-      "date": "2026-08-28"
+      "title": "练习虽丑勤耕不辍",
+      "url": "https://wlj.me/notes/sp-note-20260829-231838/",
+      "date": "2026-08-29"
     }
   ],
   "2002": [
     {
       "name": "seth",
       "year": "02",
-      "title": "What’s next?",
-      "url": "https://seths.blog/2026/08/whats-next-3/",
-      "date": "2026-08-28"
+      "title": "Fit and Finish",
+      "url": "https://seths.blog/2026/08/fit-and-finish-3/",
+      "date": "2026-08-29"
     }
   ],
   "2004": [
     {
-      "name": "TonyBai",
+      "name": "kaix.in",
       "year": "04",
-      "title": "你真的需要一座软件工厂吗？",
-      "url": "https://tonybai.com/2026/08/29/do-your-need-a-software-factory/",
-      "date": "2026-08-28"
+      "title": "那时候，贾母也才十来岁",
+      "url": "https://kaix.in/2026/0829/",
+      "date": "2026-08-29"
     },
     {
       "name": "我的天",
@@ -36,91 +36,91 @@ const latestArticlesByYear = {
       "title": "瞎扯淡",
       "url": "http://www.xlanda.net/posts/22430",
       "date": "2026-08-29"
-    },
-    {
-      "name": "小猪的窝",
-      "year": "04",
-      "title": "老花",
-      "url": "https://ezo.biz/Diary/1624.html",
-      "date": "2026-08-28"
     }
   ],
   "2005": [
     {
-      "name": "ACEVS",
+      "name": "王志勇",
       "year": "05",
-      "title": "定时重启路由发现断连",
-      "url": "https://acevs.com/5195/",
-      "date": "2026-08-28"
+      "title": "流畅的系统和火狐浏览器",
+      "url": "http://www.auiou.com/relevant/00002185.jsp",
+      "date": "2026-08-29"
     },
     {
       "name": "Gea-Suan Lin",
       "year": "05",
-      "title": "Python 的 httpx 的狀況",
-      "url": "https://blog.gslin.org/archives/2026/08/29/13167/python-%e7%9a%84-httpx-%e7%9a%84%e7%8b%80%e6%b3%81/",
+      "title": "PHP 8 上的 Taint",
+      "url": "https://blog.gslin.org/archives/2026/08/29/13168/php-8-%e4%b8%8a%e7%9a%84-taint/",
+      "date": "2026-08-29"
+    },
+    {
+      "name": "ACEVS",
+      "year": "05",
+      "title": "碎片2026年8月29日",
+      "url": "https://acevs.com/5197/",
+      "date": "2026-08-29"
+    },
+    {
+      "name": "当下",
+      "year": "05",
+      "title": "1-8月购物日志",
+      "url": "https://blog.fueis.com/26-beauty",
       "date": "2026-08-29"
     }
   ],
-  "2009": [
+  "2007": [
     {
-      "name": "张鑫旭",
-      "year": "09",
-      "title": "超级Web特性HTML-in-Canvas初体验",
-      "url": "https://www.zhangxinxu.com/wordpress/2026/08/html-in-canvas/",
-      "date": "2026-08-28"
+      "name": "朱小呆",
+      "year": "07",
+      "title": "八月的光，被我压进了延时里",
+      "url": "https://zhujay.com/blog/blog_detail.html?id=1275",
+      "date": "2026-08-29"
     }
   ],
-  "2013": [
+  "2008": [
     {
-      "name": "林羽凡",
-      "year": "13",
-      "title": "林羽凡笔记系统增加了手机号验证相关功能，增加了邮箱注册账号功能",
-      "url": "https://www.linyufan.com/post/5975",
-      "date": "2026-08-28"
+      "name": "军",
+      "year": "08",
+      "title": "出门一趟累趴了",
+      "url": "https://me.xu19.com/nine-day-beijing-trip-exhausted/",
+      "date": "2026-08-29"
+    }
+  ],
+  "2012": [
+    {
+      "name": "水拍石",
+      "year": "12",
+      "title": "《现代世界体系》简评",
+      "url": "https://www.leitao.cn/10768.html",
+      "date": "2026-08-29"
     }
   ],
   "2015": [
     {
-      "name": "满心",
+      "name": "网工格物",
       "year": "15",
-      "title": "Deepseek Harness有点儿东西",
-      "url": "https://zhoutian.com/394.html",
-      "date": "2026-08-28"
-    },
-    {
-      "name": "WordPress 知识宝库",
-      "year": "15",
-      "title": "Vultr HestiaCP 워드프레스 SSL 보안 인증서 오류 해결 방법",
-      "url": "https://www.thewordcracker.com/intermediate/vultr-hestiacp-ssl-%ec%9d%b8%ec%a6%9d%ec%84%9c-%ec%98%a4%eb%a5%98-%ed%95%b4%ea%b2%b0/",
-      "date": "2026-08-28"
+      "title": "VMware VCF 9.1 下载和激活",
+      "url": "https://songxwn.com/VMware-VCF9.1-Download/",
+      "date": "2026-08-29"
     }
   ],
   "2017": [
     {
-      "name": "碎言",
-      "year": "17",
-      "title": "博客阅读随感 关于宠物",
-      "url": "https://www.suiyan.cc/blog/20260828160554",
-      "date": "2026-08-28"
-    },
-    {
       "name": "串串狗小刊",
       "year": "17",
-      "title": "牛来大模型 Ox Alpha 此前是怎么被发现是 GLM 的？",
-      "url": "https://www.ccgxk.com/emlog_dev/862.html",
-      "date": "2026-08-28"
-    }
-  ],
-  "2019": [
-    {
-      "name": "新世界的大门",
-      "year": "19",
-      "title": "2026-07-06 / 移动路由器",
-      "url": "https://blog.xinshijiededa.men/daily/69/",
-      "date": "2026-08-28"
+      "title": "Web 出海之部署 H3 视频模型的 2 个 API 的站",
+      "url": "https://www.ccgxk.com/codeother/863.html",
+      "date": "2026-08-29"
     }
   ],
   "2020": [
+    {
+      "name": "呆哥",
+      "year": "20",
+      "title": "服务器安全更新通知",
+      "url": "https://www.dalao.net/thread-63206.htm",
+      "date": "2026-08-29"
+    },
     {
       "name": "不凡",
       "year": "20",
@@ -131,19 +131,42 @@ const latestArticlesByYear = {
   ],
   "2021": [
     {
+      "name": "ovo",
+      "year": "21",
+      "title": "Cloudflare 为 1.1.1.1 瘦身：不换硬件省下 100TB 内存，DNS 查询延迟降低 19%",
+      "url": "https://ovo.cc/archives/1440/",
+      "date": "2026-08-29"
+    },
+    {
       "name": "DevNow",
       "year": "21",
-      "title": "Product Hunt 每日热榜 | 2026-08-28",
-      "url": "https://www.laughingzhu.cn/posts/ph-daily-2026-08-28",
-      "date": "2026-08-28"
+      "title": "Product Hunt 每日热榜 | 2026-08-29",
+      "url": "https://www.laughingzhu.cn/posts/ph-daily-2026-08-29",
+      "date": "2026-08-29"
     }
   ],
   "2023": [
     {
       "name": "鹿泽",
       "year": "23",
-      "title": "必须优化的12个关键页面SEO元素",
-      "url": "https://www.bailuze.com/24382.html",
+      "title": "什么是SEO服务？ – SEO所有技巧和窍门",
+      "url": "https://www.bailuze.com/24383.html",
+      "date": "2026-08-29"
+    },
+    {
+      "name": "Siwei Li",
+      "year": "23",
+      "title": "AirPods Pro 3 还是 AirPods 4 降噪版？",
+      "url": "https://siwei.li/airpods-pro-3-vs-airpods-4-anc/",
+      "date": "2026-08-29"
+    }
+  ],
+  "2025": [
+    {
+      "name": "Marcus",
+      "year": "25",
+      "title": "中医理论",
+      "url": "https://www.immarcus.com/blog/chinese-medicine",
       "date": "2026-08-29"
     }
   ]
